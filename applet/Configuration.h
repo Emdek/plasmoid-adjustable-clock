61b4082c7a31bb08b74847a4462f954459841648
Michał Dutkiewicz <emdeck@gmail.com>
2013-03-09 20:26:36 +0100

    Update TODO

--- applet/Configuration.h
+++ applet/Configuration.h
@@ -36,7 +36,6 @@ namespace AdjustableClock
 enum ModelRole { IdRole = (Qt::UserRole + 1), TitleRole = (Qt::UserRole + 2), DescriptionRole = (Qt::UserRole + 3), AuthorRole = (Qt::UserRole + 4), HtmlRole = (Qt::UserRole + 5), CssRole = (Qt::UserRole + 6), ScriptRole = (Qt::UserRole + 7), BackgroundRole = (Qt::UserRole + 8), BundledRole = (Qt::UserRole + 9) };
 
 class Applet;
-class FormatLineEdit;
 
 class Configuration : public QObject
 {
