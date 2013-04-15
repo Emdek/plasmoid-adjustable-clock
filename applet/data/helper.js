2c164815299e6093a062ea647c2b6af5726c5289
Michał Dutkiewicz <emdeck@gmail.com>
2013-04-15 14:20:14 +0200

    Add options to Ring Clock theme

--- applet/data/helper.js
+++ applet/data/helper.js
@@ -8,6 +8,11 @@ Clock.setStyle = function (object, property, value)
 	object.style.setProperty(property, value);
 }
 
+Clock.setRuleStyle = function (rule, property, value)
+{
+	document.styleSheets[0].cssRules[rule].style.setProperty(property, value);
+}
+
 Clock.getStyle = function (object, property)
 {
 	return window.getComputedStyle(object).getPropertyValue(property);
