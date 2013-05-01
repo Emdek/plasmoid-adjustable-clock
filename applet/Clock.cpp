cf4716648a75f210c23f8efa24469beae2f3db59
Michał Dutkiewicz <emdeck@gmail.com>
2013-05-01 22:25:24 +0200

    Return empty holidays string for constant clocks

--- applet/Clock.cpp
+++ applet/Clock.cpp
@@ -339,7 +339,7 @@ QVariant Clock::getValue(ClockComponent component, const QVariantMap &options, b
 
         return QString();
     case HolidaysComponent:
-        return (options.contains("short") ? (m_holidays.isEmpty() ? QString() : m_holidays.last()) : m_holidays.join("<br>\n"));
+        return (constant ? QString() : (options.contains("short") ? (m_holidays.isEmpty() ? QString() : m_holidays.last()) : m_holidays.join("<br>\n")));
     case SunriseComponent:
         return KGlobal::locale()->formatTime(m_sunrise, false);
     case SunsetComponent:
